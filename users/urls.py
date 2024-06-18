08be1dc0e2c63d58f4c8055a90926b20cc785c07
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-18 16:19:07 +0300

    Fix logout view getting wrong request method

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 """Defines URL patterns for users"""
 
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "users"
@@ -9,4 +10,6 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     # Registration page
     path("register/", views.register, name="register"),
+    # Logout page
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
