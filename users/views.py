4f9f503b27b76ee7e666271e8e5165564f7350af
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-18 13:24:39 +0300

    Add register route

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("vault")
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
