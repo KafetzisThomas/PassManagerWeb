8310aecd9f0255b1df458963cc90ffb34b6a46eb
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-18 13:13:51 +0300

    Add logout route

--- main/settings.py
+++ main/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGOUT_REDIRECT_URL = "/vault"
