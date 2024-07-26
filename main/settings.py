88cac5f919f8384b8da2c7a7b0845fd9003ca4fd
KafetzisThomas <tomkafetzis06@gmail.com>
2024-07-26 09:21:10 +0300

    Fix typo in settings

--- main/settings.py
+++ main/settings.py
@@ -105,7 +105,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
+            "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
             "HOST": os.getenv("DB_HOST"),
