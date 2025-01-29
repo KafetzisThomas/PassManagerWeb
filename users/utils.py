be5b2ca193b2efc4e3614881fd8e1006fbf88c53
KafetzisThomas <tomkafetzis06@gmail.com>
2025-01-29 19:43:18 +0200

    Make recipient list dynamic in new_user_registration email

--- users/utils.py
+++ users/utils.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 def send_new_user_registration(user):
     subject = "Admin Notification: New User Registration"
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ["passmanagerweb@gmail.com"]
+    recipient_list = [settings.EMAIL_HOST_USER]
 
     html_message = render_to_string(
         "email_templates/new_user_registration.html",
