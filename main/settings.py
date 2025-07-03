201d3daff6e5434ff25304704a47e3ce99d6b8a2
KafetzisThomas <tomkafetzis06@gmail.com>
2025-07-03 11:20:44 +0300

    Use a dummy key for dependabot

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "test_dummy_secret")  # Use a dummy key for dependabot
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
