d2b8fdddda3589517485923838f6d0254a8bd6c8
KafetzisThomas <tomkafetzis06@gmail.com>
2026-05-26 21:01:26 +0300

    Remove unnecessary login call from reauth decorator

--- passmanager/decorators.py
+++ passmanager/decorators.py
@@ -1,15 +1,10 @@
-from django.contrib.auth import login
 from django.shortcuts import render
 from users.forms import PasswordConfirmationForm
 
 def reauth_required(view_func):
-    """
-    Force master password re-authentication before accessing the view.
-    """
     def wrapper(request, *args, **kwargs):
         form = PasswordConfirmationForm(user=request.user, data=request.POST or None)
         if request.method == "POST" and form.is_valid():
-            login(request, request.user, backend="users.backends.EmailBackend")  # refresh session
             return view_func(request, *args, **kwargs)
 
         return render(request, "users/master_password_prompt.html", {"form": form})
