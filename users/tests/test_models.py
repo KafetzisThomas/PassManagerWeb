6858d2e672b03f8018723aef9e2a10ce5f3f2b9e
KafetzisThomas <tomkafetzis06@gmail.com>
2026-05-26 17:52:59 +0300

    Remove framework level tests from user models

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,51 +1,23 @@
-from django.contrib.auth import get_user_model
-from django.db.utils import IntegrityError
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class CustomUserModelTests(TestCase):
-    """
-    Test suite for the CustomUser model.
-    """
+
     def setUp(self):
-        self.user_model = get_user_model()
-        self.user_data = {
-            "email": "tester@example.com","username": "tester","password": "password123",
-            "otp_secret": "12345678901234567890123456789012", "session_timeout": 300, "enable_2fa": False,
-        }
+        self.user = User.objects.create_user(email="user@test.com", username="user", password="Str0ng_p@ssword")
 
     def test_valid_user_creation(self):
-        """
-        Test that a user can be created with the given data.
-        """
-        user = self.user_model.objects.create_user(**self.user_data)
-        self.assertEqual(user.email, self.user_data["email"])
-        self.assertEqual(user.username, self.user_data["username"])
-        self.assertTrue(user.check_password(self.user_data["password"]))
-        self.assertEqual(user.otp_secret, self.user_data["otp_secret"])
-        self.assertEqual(user.session_timeout, self.user_data["session_timeout"])
-        self.assertEqual(user.enable_2fa, self.user_data["enable_2fa"])
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(self.user.email, "user@test.com")
+        self.assertTrue(self.user.check_password("Str0ng_p@ssword"))
+        self.assertIsNotNone(self.user.encryption_salt)
+        self.assertEqual(len(self.user.encryption_salt), 44)
 
-    def test_email_unique(self):
-        """
-        Test that the email field is unique.
-        """
-        self.user_model.objects.create_user(**self.user_data)
-        with self.assertRaises(IntegrityError):
-            self.user_model.objects.create_user(**self.user_data)
-
-    def test_otp_secret_field(self):
-        """
-        Test that the otp_secret field is correctly set.
-        """
-        user = self.user_model.objects.create_user(**self.user_data)
-        self.assertEqual(user.otp_secret, self.user_data["otp_secret"])
-        self.assertEqual(len(user.otp_secret), 32)
-
-    def test_salt_generation(self):
-        """
-        Test that salt is automatically generated if not provided.
-        """
-        data = self.user_data.copy()
-        user = self.user_model.objects.create_user(**data)
-        self.assertIsNotNone(user.encryption_salt)
+    def test_save_preserves_existing_encryption_salt(self):
+        original_salt = self.user.encryption_salt
+        self.user.username = "new_username"
+        self.user.save()
+        self.assertEqual(self.user.encryption_salt, original_salt)
