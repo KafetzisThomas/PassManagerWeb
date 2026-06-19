9c0f6eaea19d46fdd75fc15af03ca10140eb85e0
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-19 19:39:29 +0300

    Use whitenoise for static files instead of S3

--- main/settings.py
+++ main/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -145,24 +146,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if not DEBUG:
-    # prod: use S3 for static files
-    STORAGES = {
-        "staticfiles": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        },
-    }
-    AWS_ACCESS_KEY_ID = os.getenv("STORAGE_ACCESS_KEY")
-    AWS_SECRET_ACCESS_KEY = os.getenv("STORAGE_SECRET_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
-    AWS_S3_ENDPOINT_URL = os.getenv("STORAGE_ENDPOINT_URL")
-    AWS_S3_REGION_NAME = os.getenv("STORAGE_REGION_NAME")
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
